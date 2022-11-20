409bfc28311f2edf987ffd6aa3983f842b157588
FierBlox <fierblox1@mail.ru>
2022-11-20 23:11:59 +0300

    Научился рассказывать анекдоты Научился отвечать погоду (В Твери) Добавлены новые команды

--- functions.py
+++ functions.py
@@ -1,17 +1,21 @@
-import webbrowser, sys, requests, pyttsx3
+import webbrowser, pyttsx3, requests, sys, pyjokes
+
+from textblob import TextBlob
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('rate', 180)
 engine.setProperty('voice', voices[0].id)
 
-
 def speaker(text):
     engine.say(text)
     engine.runAndWait()
 
 def weather():
-    pass
+ params = {'q': 'Tver', 'units': 'metric', 'lang': 'ru', 'appid': '0492c630be20232aa9f77c238272d638'}
+ response = requests.get(f'https://api.openweathermap.org/data/2.5/weather', params=params)
+ w = response.json()
+ speaker(f"На улице {w['weather'][0]['description']} {round(w['main']['temp'])} градусов" )
 
 def browser():
     webbrowser.open("google.com", new=2)
@@ -20,7 +24,11 @@ def neutral():
     pass
 
 def joke():
-    print("Колобок повесился")
+ joke = pyjokes.get_joke()
+ print(joke)
+ blob = TextBlob(joke)
+ end = blob.translate(from_lang='en', to='ru')
+ speaker(end)
 
 def offbot():
     sys.exit()
