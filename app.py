4cef2a405023cc54dbe967d67ab4c6571e4453ab
FierBlox <fierblox1@mail.ru>
2023-04-27 22:40:52 +0300

    Добавил комментарии

--- app.py
+++ app.py
@@ -11,28 +11,33 @@ q = queue.Queue()
 
 model = vosk.Model('model_small')
 
-device = sd.default.device    
-samplerate = int(sd.query_devices(device[0], 'input')['default_samplerate']) 
+device = sd.default.device   #устройства ввода и вывода по умолчанию
+samplerate = int(sd.query_devices(device[0], 'input')['default_samplerate'])  #частота дискретизации
 
 
+#запись в очередь речи, если callback переполнился
 def callback(indata, frames, time, status):
     q.put(bytes(indata))
 
 
 def main():
 
+    #векторное представление ключей
     vectoraizer = CountVectorizer()
     vectors = vectoraizer.fit_transform(list(commands.data_set.keys()))
 
+    #соотношение ключей и значений
     clf = LogisticRegression()
     clf.fit(vectors, list(commands.data_set.values()))
 
     del commands.data_set
 
-
+    #класс записывающий речь и обрабатывающий её, если человек перестал говорить
     with sd.RawInputStream(samplerate=samplerate, blocksize=16000, device=device[0], dtype='int16', channels=1, callback=callback):
         global data
         rec = vosk.KaldiRecognizer(model, samplerate)
+
+        #бесконечный цикл записи речи
         while True:
             data = q.get()
             if rec.AcceptWaveform(data):
@@ -40,14 +45,22 @@ def main():
              recognize(data, vectoraizer, clf)
 
 
+#функция вызывающая соответсвующую функцию
 def recognize(data, vectoraizer, clf):
+
+    #проверка на имя голосового помощника
     name = commands.NAME.intersection(data.split())
     if not name:
         return
+    
+    #замена имени на пустую строку
     data.replace(list(name)[0], "")
+
+    #представление речи в векторах и их сравнение
     text_vector = vectoraizer.transform([data]).toarray()[0]
     answer = clf.predict([text_vector])[0]
     
+    #вызов функции
     func_name = answer.split()[0]
     speaker(answer.replace(func_name, ""))
     exec(func_name + "()")
--- functions.py
+++ functions.py
@@ -3,30 +3,41 @@ from jokes import jokes
 from random import randint
 import webbrowser, pyttsx3, requests, sys
 
+#настройки голоса помощника
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('rate', 180)
 engine.setProperty('voice', voices[0].id)
 
+#функция озвучивания речи
 def speaker(text):
     engine.say(text)
     engine.runAndWait()
 
+#функция, узнающая погоду
 def weather():
  params = {'q': 'Tver', 'units': 'metric', 'lang': 'ru', 'appid': api}
  response = requests.get(f'https://api.openweathermap.org/data/2.5/weather', params=params)
+
+ #предтавление данных в виде списка
  w = response.json()
  speaker(f"На улице {w['weather'][0]['description']} {round(w['main']['temp'])} градусов" )
 
+#открытие браузра по умолчанию
 def browser():
     webbrowser.open("google.com", new=2)
 
+#пустая функция, озвучивает то, что прописанов значения к ключам
 def neutral():
     pass
 
+#функция рассказывающая шутку
 def joke():
+ 
+ #выбор случайного индекса в списке шуток
  index = randint(0, len(jokes) - 1)
  speaker(jokes[index])
 
+#выключение бота
 def offbot():
     sys.exit()
